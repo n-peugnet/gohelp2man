8fe6134f79fa0f74461fcfc76c1db018c1a4d3ae
Nicolas Peugnet <nicolas@club1.fr>
2025-11-06 13:32:36 +0100

    Also use -opt-include in testdata/update.sh

--- testdata/update.sh
+++ testdata/update.sh
@@ -3,5 +3,5 @@ export SOURCE_DATE_EPOCH=0
 set -x
 for f in testdata/test_full_*.txt
 do
-	GOHELP2MAN_TESTCASE=$f go run . -include "${f%.txt}.h2m" "testdata/test.sh" > "${f%.txt}.1"
+	GOHELP2MAN_TESTCASE=$f go run . -opt-include "${f%.txt}.h2m" "testdata/test.sh" > "${f%.txt}.1"
 done
