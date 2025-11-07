a580346310af716bb9e04e0b5da8fa8f8c04a3ea
Nicolas Peugnet <nicolas@club1.fr>
2025-11-07 18:04:08 +0100

    Uniformise GPL headers in all *.go files

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
-// gohelp2man, generate a man page out of a Go program's -help output
+// This file is part of gohelp2man.
 //
 // Copyright (C) 2025  Nicolas Peugnet <nicolas@club1.fr>
 //
-// This program is free software; you can redistribute it and/or
+// gohelp2man is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// gohelp2man is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
--- regexp_replacer.go
+++ regexp_replacer.go
@@ -1,4 +1,4 @@
-// This file is a part of gohelp2man
+// This file is part of gohelp2man.
 //
 // Copyright (C) 2025  Nicolas Peugnet <nicolas@club1.fr>
 //
@@ -7,7 +7,7 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// gohelp2man is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
